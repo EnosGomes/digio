733c0bca06ffc217b14ffa3548928a4baa3e0811
enosgomes <enosgomesdev@gmail.com>
2025-11-20 13:47:22 -0300

    Commit inicial

---
+++ src/main/java/org/example/msdigio/MsDigioApplication.java
@@ -0,0 +1,13 @@
+package org.example.msdigio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsDigioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsDigioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/msdigio/MsDigioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.msdigio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsDigioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
